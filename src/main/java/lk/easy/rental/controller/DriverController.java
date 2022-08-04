25dcae401cd93897fc6ed900efd42ee809be1144
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-08-04 13:01:37 +0530

    Driver Image Delete Done

--- src/main/java/lk/easy/rental/controller/DriverController.java
+++ src/main/java/lk/easy/rental/controller/DriverController.java
@@ -3,10 +3,15 @@ package lk.easy.rental.controller;
 import lk.easy.rental.dto.DriverDTO;
 import lk.easy.rental.service.DriverService;
 import lk.easy.rental.util.ResponseUtil;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author : Yasiru Dahanayaka
@@ -57,5 +62,18 @@ public class DriverController {
     @GetMapping(params = {"test"},produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil generateDriverIds(@RequestParam String test) {
         return new ResponseUtil(200, "Ok", driverService.generateDriverIds());
+    }
+
+    @SneakyThrows
+    @DeleteMapping(path = "deleteDriverImage", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteAllCustomerImages(@RequestParam String id) {
+        String pathDirectory = "F:\\Project\\Web-Second Sem\\Spring Projects\\Easy Car Rental System\\Car_Rental_Backend\\src\\main\\resources\\static\\image\\registerImage\\";
+
+        String[] registerImageView = {"Nic", "Licence"};
+
+        for (int i = 0; i < registerImageView.length; i++) {
+            Files.deleteIfExists(Paths.get(pathDirectory + File.separator + id + registerImageView[i] + ".jpeg"));
+        }
+        return new ResponseUtil(200, "Driver Data Delete success", null);
     }
 }
